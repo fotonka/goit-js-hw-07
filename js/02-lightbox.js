5b9da71c80da98c51bdcb13237ea18481612517c
Kirill Chaialo <fotonka098@gmail.com>
2022-07-18 16:31:39 +0300

    2nd_ready

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGallery(e) {
+  return e
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+		<img class="gallery__image" src="${preview}" alt="${description}" />
+	</a>`;
+    })
+    .join("");
+}
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+gallery.on("show.simplelightbox");
